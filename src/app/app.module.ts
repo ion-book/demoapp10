859e7260ccfd1f687fdd6c935286c1cd73571fbd
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-06-21 16:16:28 -0500

    installing firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireModule} from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "<your-key>",
+    authDomain: "<your-project-authdomain>",
+    databaseURL: "<your-database-url>",
+    storageBucket: "<your-storage-bucket>",
+    messagingSenderId: "<your-messaging-sender-id>"
+  };
 
 @NgModule({
   declarations: [
@@ -10,7 +21,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
